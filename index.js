c4e67498d5c1c4d0bd45b07f5927d1f55cf545bc
Henry <henrysungyun@gmail.com>
2023-02-05 15:18:46 -0800

    come back to this

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        }),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            const newId = data.id;
+            const element = document.createElement('div');
+            element.innerHTML = `New ID: ${newId}`;
+            document.body.appendChild(element);
+        })
+        .catch(function(error) {
+            const element = document.createElement('div');
+            element.innerHTML = `Error: ${error.message}`;;
+            document.body.appendChild(element);
+        })
+}
